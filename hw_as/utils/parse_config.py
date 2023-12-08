646133d7efd2a4eb5af07555d89c1ae89bf75c53
Kirill Korolev <kakorolev@edu.hse.ru>
2023-12-08 17:31:06 +0000

    full training kaggle config

--- hw_as/utils/parse_config.py
+++ hw_as/utils/parse_config.py
@@ -150,13 +150,13 @@ class ConfigParser:
 
     @classmethod
     def get_default_configs(cls):
-        config_path = ROOT_PATH / "hw_tts" / "config.json"
+        config_path = ROOT_PATH / "hw_as" / "config.json"
         with config_path.open() as f:
             return cls(json.load(f))
 
     @classmethod
     def get_test_configs(cls):
-        config_path = ROOT_PATH / "hw_tts" / "tests" / "config.json"
+        config_path = ROOT_PATH / "hw_as" / "tests" / "config.json"
         with config_path.open() as f:
             return cls(json.load(f))
 
